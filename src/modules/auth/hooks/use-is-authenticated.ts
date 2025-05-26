02c9a6f40dab2d503dbd8c06384a4eebebdcb2cc
ELDIABE22 <jeronimo.jsa.133@gmail.com>
2025-05-26 12:42:08 -0500

    fix: ajustar configuración de ESLint y corregir lógica de autenticación en el login

--- src/modules/auth/hooks/use-is-authenticated.ts
+++ src/modules/auth/hooks/use-is-authenticated.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from '@auth/stores/auth'
 export function useIsAuthenticated() {
   const accessToken = useAuthStore(state => state.accessToken)
   const user = useAuthStore(state => state.user)
-  // const isAuthenticated = accessToken !== undefined && !!user
-  const isAuthenticated = true
+  const isAuthenticated = accessToken !== undefined && !!user
+  // const isAuthenticated = false
   return isAuthenticated
 }
